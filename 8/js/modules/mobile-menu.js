29f2a5ef05e7a32b43baebb2a49b5b73cc6d3794
Keks <keks@htmlacademy.ru>
2022-05-06 10:36:28 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/modules/mobile-menu.js
@@ -0,0 +1,16 @@
+const navMain = document.querySelector('.navigation__list');
+const navToggle = document.querySelector('.navigation__button');
+
+navMain.classList.remove('navigation__list--no-js');
+navMain.classList.add('navigation__list--closed');
+navToggle.classList.remove('navigation__button--no-js');
+
+navToggle.addEventListener('click', () => {
+  navToggle.classList.toggle('navigation__button--opened');
+  navMain.classList.toggle('navigation__list--closed');
+  if (navToggle.classList.contains('navigation__button--opened')) {
+    navToggle.querySelector('span').textContent = 'Закрыть меню навигации';
+  } else {
+    navToggle.querySelector('span').textContent = 'Открыть меню навигации';
+  }
+});
